7a230bdb85ef65ef012478fbdbbe3dadafae8b1c
sunimalherath <sunimaldv@gmail.com>
2020-09-30 12:12:37 +1000

    getting data from server

--- Tutorials/Services/HTTPService.h
+++ Tutorials/Services/HTTPService.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface HTTPService : NSObject
 
 + (id) instance;
+- (void) getTutorials;
 
 @end
 
--- Tutorials/Services/HTTPService.m
+++ Tutorials/Services/HTTPService.m
@@ -7,6 +7,9 @@
 
 #import "HTTPService.h"
 
+#define URL_BASE "http://localhost:6069"
+#define URL_TUTORIALS "/tutorials"
+
 @implementation HTTPService
 
 + (id) instance {
@@ -17,5 +20,22 @@
         }
     }
     return sharedInstance;
+}
+
+- (void) getTutorials {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%s%s", URL_BASE, URL_TUTORIALS]];
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (data != nil) {
+            NSError *err;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+            
+            if (err == nil) {
+                NSLog(@"JSON: %@",json.debugDescription);
+            }
+        } else {
+            NSLog(@"Err: %@", error.debugDescription);
+        }
+    }] resume];
 }
 @end
